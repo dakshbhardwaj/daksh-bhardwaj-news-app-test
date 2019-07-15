24274e1de79fa926fc74f37e25ecc49ccb8a25c6
dakshbhardwaj2@gmail.com <dakshbhardwaj2@gmail.com>
2019-07-15 22:46:09 +0530

    first-commit

---
+++ app/modules/home/constants.js
@@ -0,0 +1,9 @@
+
+export const NAME = 'home';
+
+export const RETRIEVING_HEADLINES = 'home/RETRIEVING_HEADLINES';
+export const HEADLINES_AVAILABLE = 'home/HEADLINES_AVAILABLE';
+export const HEADLINES_ERROR = 'home/HEADLINES_ERROR';
+
+export const API_URL = 'https://newsapi.org/v2/top-headlines?';
+export const API_KEY = "9b64bcfe576047ba8e5bb7fd24c9e526";
